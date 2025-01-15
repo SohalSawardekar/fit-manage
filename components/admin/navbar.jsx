f16f9f6b7901e37305eb50f353179d3751ac9183
SohalSawardekar <sohalsawardekar11@gmail.com>
2025-01-15 18:29:58 +0530

    first commit

--- components/admin/navbar.jsx
+++ components/admin/navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Image from "@node_modules/next/image";
 import Link from "@node_modules/next/link";
+import { Button } from "@components/ui/button";
+import { signOut } from "@node_modules/next-auth/react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[5rem] bg-slate-300 flex flex-row justify-evenly items-center">
+    <div className="w-full h-[5rem] bg-slate-300 flex flex-row justify-around items-center">
       <Link href="/admin/dashboard">
         <Image
           src="/logo/logo.png"
@@ -15,6 +17,10 @@ const Navbar = () => {
         />
       </Link>
       <h1 className="font-bold text-slate-700 text-2xl">Admin Page</h1>
+
+      <Button className="bg-slate-600" onClick={signOut}>
+        Log Out
+      </Button>
     </div>
   );
 };
